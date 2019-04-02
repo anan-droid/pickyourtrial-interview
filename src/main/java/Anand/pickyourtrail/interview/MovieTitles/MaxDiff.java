b60e79c51b42b2fbc6641279824062289bbafdfc
anan-droid <anan-droid@github.com>
2019-04-02 17:08:04 +0530

    modified solution for maximum diff in an array

--- src/main/java/Anand/pickyourtrail/interview/MovieTitles/MaxDiff.java
+++ src/main/java/Anand/pickyourtrail/interview/MovieTitles/MaxDiff.java
@@ -9,23 +9,41 @@ public class MaxDiff {
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in); 
-		int a = in.nextInt(); 
-		List<Integer> arr = new ArrayList<Integer>();
-		for(int i=0;i<a;i++) {
-			arr.add(in.nextInt());
+		int size = in.nextInt(); 
+		List<Integer> a = new ArrayList<Integer>();
+		for(int i=0;i<size;i++) {
+			a.add(in.nextInt());
 		}
-		System.out.println(arr);
-		int maxdiff=-1;
-		for(int i=1;i<arr.size();i++) {
-			for(int j=i-1;j>=0;j--) {
-				if(arr.get(i)>arr.get(j)) {
-					int diff=arr.get(i)-arr.get(j);
-					if(diff>maxdiff) {
-						maxdiff = diff;
-					}
-				}				
-			}
-		}
-		System.out.println("the maximum difference is:"+ maxdiff);
+		System.out.println(a);
+		int min = a.get(0);
+        int max = a.get(0);
+        int maxDiff = max-min;
+        int minTemp = a.get(0);
+        int maxTemp = a.get(0);
+        int diffTemp = maxTemp - minTemp;
+        for (int i = 1; i < a.size(); i++) {
+            if(minTemp==min && a.get(i)>max) {
+                max = a.get(i);
+                maxDiff = max - min;
+            }                        
+            if(a.get(i)<min && a.get(i)<minTemp) {
+                minTemp = a.get(i);
+            }
+            if(minTemp!=min && a.get(i)>minTemp) {
+                maxTemp = a.get(i);
+                diffTemp = maxTemp - minTemp;
+                if (diffTemp > maxDiff) {
+                    max = maxTemp;
+                    min = minTemp;
+                    maxDiff = diffTemp;
+                }
+            }
+        }
+        if(maxDiff==0) {
+        	System.out.println("the maximum difference is: -1");
+        }
+        else {
+        	System.out.println("the maximum difference is:"+ maxDiff);
+        }	
 	}	
 }	
